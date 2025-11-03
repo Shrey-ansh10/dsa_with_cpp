9c379d1b8cea19e52a2a142a143065491249ecf4
Shreyansh <shreyanshsharma1010@gmail.com>
2025-11-03 10:39:41 -0800

    merge sort algorithm

---
+++ recursion/mergeSort.cpp
@@ -0,0 +1,69 @@
+// Implementing merge sort algorithm
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+void mergeSort(vector<int>& arr, int st, int mid, int end){
+    vector<int> temp;
+    int i=st, j=mid+1;
+
+    while(i<=mid && j<=end){
+        if(arr[i] < arr[j]){
+            temp.push_back(arr[i]);
+            i++;
+        }else if(arr[j] < arr[i]){
+            temp.push_back(arr[j]);
+            j++;
+        }
+        else{
+            temp.push_back(arr[i]);
+            temp.push_back(arr[j]);
+            i++, j++;
+        }
+    }
+
+    // if one part is traversed and other is not
+    while(i<=mid){
+        temp.push_back(arr[i]);
+        i++;
+    }
+    while(j<=end){
+        temp.push_back(arr[j]);
+        j++;
+    }
+
+    //copying sorted elements to original array
+    for(int idx=0; idx<temp.size(); idx++){
+        arr[st+idx] = temp[idx];
+    }
+}
+
+vector<int> divide(vector<int>& arr, int st, int end){
+    if(st<end){
+        int mid = st + (end-st)/2 ;
+
+        divide(arr, st, mid); // left half
+
+        divide(arr, mid+1, end); //right half
+
+        mergeSort(arr, st, mid, end); // merge
+
+    }
+    return arr;
+}
+
+
+
+int main(){
+
+    vector<int> arr = {10, 2, 8, 6, 7, 3, 1, 5, 4};
+    divide(arr, 0, arr.size()-1);
+
+    for(int i : arr){ //print sorted array
+        cout << i << " " << endl;
+    }
+
+    return 0;
+}
