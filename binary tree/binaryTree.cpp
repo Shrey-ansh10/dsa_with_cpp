23bf34664218c7091ea39224c9fdb2828a8b07f2
Shreyansh <shreyanshsharma1010@gmail.com>
2025-12-22 12:23:58 -0800

    Post-Order Traversal on Binary Tree

--- binary tree/binaryTree.cpp
+++ binary tree/binaryTree.cpp
@@ -49,6 +49,20 @@ void inOrderTraversal(Node* root){ // O(n)
     preOrderTraversal(root->right); // recursive call on right subtree
 }
 
+void postOrderTraversal(Node* root){ // O(n)
+
+    if(root == nullptr) return; // if root is null means this is the end, to return to parent node
+
+    preOrderTraversal(root->left); // recursive call on left subtree
+    preOrderTraversal(root->right); // recursive call on right subtree
+    cout << root->data << " "; // print the data at root
+}
+
+// Pre, In & Post defines when we travers the ROOT node
+// Pre : we traverse root - left -right. root first
+// In : we traverse left-root-right. root in in center/2nd
+// Post : traverse left-right-root. root at last
+
 int main(){
 
     vector<int> preOrder = {2, 3, 4, -1, -1, 5, -1, -1, 7, -1, -1};
