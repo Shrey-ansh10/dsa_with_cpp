93b9e4c48d614fa8007fadd96a249b3ef1469b2a
Shreyansh <shreyanshsharma1010@gmail.com>
2026-01-10 23:43:24 -0800

    LC-1123

--- binary tree/subtreeWithAllDepestNodes.cpp
+++ binary tree/subtreeWithAllDepestNodes.cpp
@@ -1,7 +1,14 @@
 // LC-865 : Smallest Subtree with all the Deepest Nodes
 // Given the root of a binary tree, the depth of each node is the shortest distance to the root.
-
 // Return the smallest subtree such that it contains all the deepest nodes in the original tree.
+
+// Same Question with different Question Statement
+// LC-1123: Lowest Common Ancestor of Deepest Leaves 
+// Given the root of a binary tree, return the lowest common ancestor of its deepest leaves.
+// Recall that:
+// The node of a binary tree is a leaf if and only if it has no children
+// The depth of the root of the tree is 0. if the depth of a node is d, the depth of each of its children is d + 1.
+// The lowest common ancestor of a set S of nodes, is the node A with the largest depth such that every node in S is in the subtree with root A.
 
 #include<iostream>
 using namespace std;
